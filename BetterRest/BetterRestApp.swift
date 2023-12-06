e4bfe3514c9ac493a4d363863d52fcb9dcfe92f5
Leo Chung <leo.chung97@gmail.com>
2023-12-06 14:15:28 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Leo Chung on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
