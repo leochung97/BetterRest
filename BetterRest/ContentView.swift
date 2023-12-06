fb11269e652ca83d68b1cd90c9ec50aeb7777e5a
Leo Chung <leo.chung97@gmail.com>
2023-12-06 18:26:34 -0500

    Challenge 3

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
-    let cups = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    let cups = Array(1...20)
 
     // Without static, this would fail to compile as we are accessing one property from inside another
     // You need static to know that defaultWakeTime belongs to the ContentView struct itself rather than a single instance of the struct -> this means defaultWakeTime can be read whenever we want
@@ -23,6 +23,28 @@ struct ContentView: View {
         components.hour = 7
         components.minute = 0
         return Calendar.current.date(from: components) ?? .now
+    }
+    
+    var defaultResult: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            return sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            showingAlert = true
+            return "Error"
+        }
     }
 
     var body: some View {
@@ -37,12 +59,10 @@ struct ContentView: View {
                 Section("Wake Up Time") {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                 }
-                .font(.headline)
                 
                 Section("Desired amount of sleep") {
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.5)
                 }
-                .font(.headline)
                 
                 Section("Daily coffee intake") {
                     // We can tell SwiftUI that the word cup needs to be "inflected" to match whatever is in the coffeeAmount variable -> this will automatically convert cup to cups if appropriate
@@ -50,20 +70,25 @@ struct ContentView: View {
                     Picker("^[\(coffeeAmount) cup](inflect: true)", selection: $coffeeAmount) {
                         ForEach(cups, id: \.self) { cup in
                             Text("\(cup)").tag(cup)
-                        }q
+                        }
                     }
                 }
-                .font(.headline)
+                
+                Section("Optimal Sleep Time:") {
+                    Text(defaultResult)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .font(.headline)
+                }
             }
             .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") {}
-            } message: {
-                Text(alertMessage)
-            }
+            // .toolbar {
+            //     Button("Calculate", action: calculateBedtime)
+            // }
+            // .alert(alertTitle, isPresented: $showingAlert) {
+            //     Button("OK") {}
+            // } message: {
+            //     Text(alertMessage)
+            // }
         }
     }
 
@@ -85,9 +110,10 @@ struct ContentView: View {
         } catch {
             alertTitle = "Error"
             alertMessage = "Sorry, there was a problem calculating your bedtime."
+            showingAlert = true
         }
 
-        showingAlert = true
+        // showingAlert = true
     }
 }
 
